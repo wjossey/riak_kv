c54023a1546174fd307c8eca5318c669e9a25fce
Joseph Blomstedt <joe@basho.com>
2012-08-17 00:23:23 -0700

    cleanup: exchange directly with pids

--- src/exchange_fsm.erl
+++ src/exchange_fsm.erl
@@ -92,19 +92,14 @@ prepare_exchange({remote_exchange, Error}, State) ->
 
 update_trees(start_exchange, State=#state{local=LocalVN,
                                           remote=RemoteVN,
+                                          local_tree=LocalTree,
+                                          remote_tree=RemoteTree,
                                           index_n=IndexN}) ->
     lager:info("Sending to ~p", [LocalVN]),
     lager:info("Sending to ~p", [RemoteVN]),
 
-    Sender = {fsm, undefined, self()},
-    riak_core_vnode_master:command(LocalVN,
-                                   {update_tree, IndexN},
-                                   Sender,
-                                   riak_kv_vnode_master),
-    riak_core_vnode_master:command(RemoteVN,
-                                   {update_tree, IndexN},
-                                   Sender,
-                                   riak_kv_vnode_master),
+    update_request(LocalTree, LocalVN, IndexN),
+    update_request(RemoteTree, RemoteVN, IndexN),
     next_state_with_timeout(update_trees, State);
 
 update_trees(timeout, State) ->
@@ -127,6 +122,8 @@ update_trees({tree_built, _, _}, State) ->
 
 key_exchange(timeout, State=#state{local=LocalVN,
                                    remote=RemoteVN,
+                                   local_tree=LocalTree,
+                                   remote_tree=RemoteTree,
                                    index_n=IndexN}) ->
     lager:info("Starting key exchange between ~p and ~p", [LocalVN, RemoteVN]),
     lager:info("Exchanging hashes for preflist ~p", [IndexN]),
@@ -142,16 +139,15 @@ key_exchange(timeout, State=#state{local=LocalVN,
     %% lager:info("R2: ~p", [R2]),
 
     Remote = fun(get_bucket, {L, B}) ->
-                     exchange_bucket(RemoteVN, IndexN, L, B);
+                     exchange_bucket(RemoteTree, IndexN, L, B);
                 (key_hashes, Segment) ->
-                     exchange_segment(RemoteVN, IndexN, Segment)
+                     exchange_segment(RemoteTree, IndexN, Segment)
              end,
 
-    R = riak_core_vnode_master:sync_command(LocalVN,
-                                            {compare_trees, IndexN, Remote},
-                                            riak_kv_vnode_master),
+    %% TODO: Do we want this to be sync? Do we want FSM to be able to timeout?
+    %%       Perhaps this should be sync but we have timeout tick?
+    KeyDiff = index_hashtree:compare(IndexN, Remote, LocalTree),
 
-    {keydiff,_,_,KeyDiff} = R,
     Missing = [binary_to_term(BKey) || {_, BKey} <- KeyDiff],
     {ok, RC} = riak:local_client(),
     [begin
@@ -175,16 +171,12 @@ key_exchange(timeout, State=#state{local=LocalVN,
 %% test(Request, State) ->
 %%     lager:info("Req: ~p", [Request]),
 %%     {next_state, test, State, 500}.
-    
-exchange_bucket(VN, IndexN, Level, Bucket) ->
-    riak_core_vnode_master:sync_command(VN,
-                                        {exchange_bucket, IndexN, Level, Bucket},
-                                        riak_kv_vnode_master).
+   
+exchange_bucket(Tree, IndexN, Level, Bucket) ->
+    index_hashtree:exchange_bucket(IndexN, Level, Bucket, Tree).
 
-exchange_segment(VN, IndexN, Segment) ->
-    riak_core_vnode_master:sync_command(VN,
-                                        {exchange_segment, IndexN, Segment},
-                                        riak_kv_vnode_master).
+exchange_segment(Tree, IndexN, Segment) ->
+    index_hashtree:exchange_segment(IndexN, Segment, Tree).
 
 handle_event(_Event, _StateName, State) ->
     %% {next_state, StateName, State}.
@@ -207,6 +199,24 @@ code_change(_OldVsn, StateName, State, _Extra) ->
 %%%===================================================================
 %%% Internal functions
 %%%===================================================================
+
+update_request(Tree, {Index, _}, IndexN) ->
+    as_event(fun() ->
+                     case index_hashtree:update(IndexN, Tree) of
+                         ok ->
+                             {tree_built, Index, IndexN};
+                         not_responsible ->
+                             {not_responsible, Index, IndexN}
+                     end
+             end).
+
+as_event(F) ->
+    Self = self(),
+    spawn(fun() ->
+                  Result = F(),
+                  gen_fsm:send_event(Self, Result)
+          end),
+    ok.
 
 do_timeout(State=#state{local=LocalVN,
                         remote=RemoteVN,
